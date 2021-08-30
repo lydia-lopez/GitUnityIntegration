8f08cf472261ed715e340c85bf792cb11c08d46f
Lydia Lopez <lopezld@mail.uc.edu>
2021-08-29 20:16:32 -0400

    Changed Hi Script
    added comment into the script to prove it's been changed

--- Assets/Hi.cs
+++ Assets/Hi.cs
@@ -7,7 +7,7 @@ public class Hi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+// comment to prove that this script has changed :)
     }
 
     // Update is called once per frame
